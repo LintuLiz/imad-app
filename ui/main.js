a88d80197f57dae319c520d72e4d908e5c6f0624
LintuLiz <30829391+LintuLiz@users.noreply.github.com>
2017-08-10 14:00:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,22 @@ var counter=0;
 button.onclick=function(){
     
     //Make a request to counter endpoint
+    var request=new XMLHttpRequest();
     
     //Capture the response and store it in a variable
-    
-    //Render the variable in the correct span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    request.onreadystatechange=function(){
+        if(current.readystate==XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status==200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+            
+        }
+        //Not done yet
+    }
+    //Make the request
+    request.open('GET','http://lintuliz.imad.hasura-ipp.io/counter',true);
+    request.send(null);
 };
