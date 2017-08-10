35480d428afe1dc1d195559e334d475494a04e97
LintuLiz <30829391+LintuLiz@users.noreply.github.com>
2017-08-10 18:41:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick=function(){
 };
 
 //Submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
    
@@ -52,6 +51,8 @@ submit.onclick=function(){
         }
         //Not done yet
     };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
     request.open('GET','http://lintuliz.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
