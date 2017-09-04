bdd2a73d60e12ff2af4d26c50f462ca8c2ac8258
LintuLiz <30829391+LintuLiz@users.noreply.github.com>
2017-09-04 16:59:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -202,6 +202,20 @@ app.get('/submit-name',function(req,res){// /submit-name?name=xxx
     res.send(JSON.stringify(names));
 });
 
+var pool = new Pool(config);
+
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
 app.get('/articles/:articleName',function(req,res){
     //articleName==article-one
     //articles[articleName]==content object for article-one
