2f3f2f6f04c169aff3170130f86793c73626b814
LintuLiz <30829391+LintuLiz@users.noreply.github.com>
2017-09-07 13:33:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
+function loadLoginForm()
+{
+    var loginHTML=`
+    <h3>Register/Login to unlock awesome features</h3>
+    <input type="text" id="username" placeholder="username"/>
+    <input type="password" id="password" placeholder="*******"/>
+    <input type="submit" value="Login" id="submit_btn"/>
+    <input type="submit" value="Register" id="register_btn"/>
+    `
+    
+    document.getElementById('login_area').innerHTML=loginHTML;
 //Submit username/password
 
 var submit=document.getElementById('submit_btn');
@@ -19,7 +30,10 @@ submit.onclick=function(){
         else if(request.status===500){
             alert('Something went wrong on the server!');
         }
-    
+        else{
+             alert('Something went wrong on the server!');
+        }
+        loadLogin();
         }
         //Not done yet
     };
@@ -30,4 +44,88 @@ submit.onclick=function(){
     request.open('POST','http://lintuliz.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
+    submit.value='Logging in';
+};  
+
+var register=document.getElementById('register_btn');
+register.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                console.log('Registered');
+                alert('User created successfully');
+                register.value='Registered';
+            }
+            else{
+                alert('Could not register the user');
+                register.value='Register';
+            }
+         }
+    };
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://lintuliz.imad.hasura-app.io/create-user',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+    register.value='Registering';
 };
+}
+
+function loadLoggedInUser()
+{
+    var loginArea=document.getElementById('login_area');
+    loginArea.innerHTML= `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+}
+
+function loadLogin(){
+    var request=new XMLHTTPRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHTTPReqeust.DONE){
+            if(request.status===200){
+                loadLoggedInUser(this.responseText);
+            }else{
+                loadLoginForm();
+            }
+        }
+    };
+    request.open('GET','/check-login', true);
+    request.send(NULL);
+}
+
+function loadArticles(){
+    var request=new XMLHTTPRequest();
+    request.onreadystatechange=function(){
+      if(request.readystate===XMLHTTPRequest.DONE){
+          if(request.state===200){
+              var content='<ul>';
+              var articleData=json.parse(this.responseText);
+              for(var i=0;i<articleData.length;i++){
+                  content+=`<li>
+                  <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                  (${articleData[i].date.split('T')[0]})</li>`;
+              }
+                content += "</ul>"
+                articles.innerHTML = content;
+          }else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+
+      }  
+    };
+request.open('GET', '/get-articles', true);
+request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
+
