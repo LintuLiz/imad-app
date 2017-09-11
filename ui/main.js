f1a64db74648be922f60e5652e007aa2fefc2476
LintuLiz <30829391+LintuLiz@users.noreply.github.com>
2017-09-11 15:45:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -88,8 +88,8 @@ function loadLogin(){
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
-           //     loadLoggedInUser(this.responseText);
-            //}else{
+              loadLoggedInUser(this.responseText);
+            }else{
                 loadLoginForm();
             }
         }
