43d91efdc145be4311071214b27edd7090ad8134
LintuLiz <30829391+LintuLiz@users.noreply.github.com>
2017-09-12 06:40:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -227,15 +227,17 @@ app.get('/get-articles', function (req, res) {
 app.get('/articles/:articleName',function(req,res){
     //articleName==article-one
     //articles[articleName]==content object for article-one
-    pool.query("SELECT * FROM article WHERE title=$1"+[req.params.articleName],function(err,result){
+    pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err.toString());
-        }else if(result.rows.length===0){
+        }else{
+            if(result.rows.length===0){
                 res.status(404).send('Article not found');
             }else{
                 var articleData=result.rows[0];
                  res.send(createTemplate(articleData));
             }
+        } 
         });
    
 });
